95fe3c7330c2fe96bd28f70262f9845fe74dc621
AlexIn.Tech <18593496+AlexIn-Tech@users.noreply.github.com>
2022-12-21 10:38:00 +0100

    Update robocopy_sync.bat

--- robocopy_sync.bat
+++ robocopy_sync.bat
@@ -5,7 +5,7 @@ set source=\\server1\share1
 set destination=\\server2\share2
 
 :: Run robocopy to copy files from the source to the destination
-robocopy %source% %destination% /MIR /Z /XJ /R:1 /W:1
+robocopy %source% %destination% /MIR /PURGE /Z /XJ /R:3 /W:30
 
 :: Display a message indicating that the file copy is complete
 echo File copy complete.
