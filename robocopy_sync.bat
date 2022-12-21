3dec6b6d230ea571ac09dc3672bbef1fc90033aa
AlexIn.Tech <18593496+AlexIn-Tech@users.noreply.github.com>
2022-12-21 12:15:46 +0100

    Update robocopy_sync.bat

--- robocopy_sync.bat
+++ robocopy_sync.bat
@@ -5,10 +5,11 @@ set source=\\server1\share1
 set destination=\\server2\share2
 
 :: Set the log file location and name
-set logfile=C:\Logs\robocopy_sync.log
+set logfile=C:\Logs\robocopy_sync_%date:~10,4%%date:~4,2%%date:~7,2%_%time:~0,2%%time:~3,2%%time:~6,2%.log
 
 :: Delete log files that are older than 3 days
-forfiles -p "C:\Logs" -s -m *.log -d -3 -c "cmd /c del /q @path"
+set logpath=C:\Logs\
+forfiles -p %logpath%% -s -m *.log -d -3 -c "cmd del @path"
 
 :: Run robocopy to copy files from the source to the destination
 robocopy %source% %destination% /MIR /PURGE /Z /XJ /R:3 /W:30 /LOG+:%logfile%
